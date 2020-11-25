e4a946ee980f790a7fffda21f3c889de75749e28
delaMorena <martadmdo@gmail.com>
2020-11-25 09:38:23 +0000

    initial commit

--- src/models.py
+++ src/models.py
@@ -8,23 +8,44 @@ from eralchemy import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    first_name = Column(String(40), nullable=False)
+    last_name = Column(String(80), nullable=False)
+    nickname = Column(String(120), nullable=False)
+    email = Column(String(120), nullable=False)
+    password = Column(String(40), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
     id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post = relationship(User)
+    image = Column(String(400), nullable=False)
+    text = Column(String(400))
+    # published_at = Column(timestamp)
+
+class Post_comment(Base):
+    __tablename__ = 'post_comment'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_id = Column(Integer, ForeignKey('post.id'))
+    post_comment = relationship(Post, User)
+    # published_at = Column(timestamp)
+    
+class Comment_likes(Base):
+    __tablename__ = 'comment_likes'
+    id = Column(Integer, primary_key=True)
+    author_id = Column(Integer, ForeignKey('user.id'))
+    post_comment_id = Column(Integer, ForeignKey('post.id'))
+    Comment_likes = relationship(Post, User) 
+
+    # published_at = Column(timestamp)
 
     def to_dict(self):
         return {}
